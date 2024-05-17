09e3d370b45bc09d46dcf47802e6562c881a0ae9
Junaied <junaied526@gmail.com>
2024-05-17 08:10:28 +0600

    responsiveness correction1 commit

--- script/script.js
+++ script/script.js
@@ -8,28 +8,4 @@ const toggleNav = () => {
 };
 mobileNav.addEventListener("click", () => toggleNav());
 
-// Routing function
-const route = (event) => {
-  event.preventDefault();
-  const path = event.target.getAttribute('href');
-  window.history.pushState({}, "", path);
-  handleLocation();
-};
 
-const routes = {
-  "/": "/index.html",
-  "/about": "/pages/about.html",
-  "/contact": "/pages/contact.html",
-};
-
-const handleLocation = async () => {
-  const path = window.location.pathname;
-  const route = routes[path] || routes[404];
-  const html = await fetch(route).then((response) => response.text());
-  document.getElementById("main-page").innerHTML = html;
-};
-
-window.onpopstate = handleLocation;
-window.route = route;
-
-handleLocation();
